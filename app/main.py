4a6875d62f96d7c4cc3714189713c1b4b12b1f99
taba1610 <kemuhi02@gmail.com>
2025-05-16 23:24:59 +0900

    add: fastAPIでのセッション最適化

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from csv_handler.csv_handler import CSVHandler
-from optimize.optimize_round import optimize_round
+# from optimize.optimize_round import optimize_round
 from optimize.bayesian_optimizer import BayesianOptimizer
 from fastapi import FastAPI
 
@@ -25,14 +25,14 @@ def round_optimizer(user_id: str, focus_score: float):
     new_data = [focus_score]
     columns = ["focus_score"]
     csv_handler.update_data(new_data=new_data, columns=columns)
-    # 説明変数と目的変数を取得
+    # ラウンド最適化の説明変数と目的変数を取得
     explanatory_variable = csv_handler.make_chosen_data_list(columns=["work_time", "break_time"])
     objective_variable = csv_handler.make_chosen_data_list(columns=["focus_score"])
     print("説明変数リスト", explanatory_variable)
     print("目的変数リスト", objective_variable)
     # ラウンド最適化
     opt = BayesianOptimizer("round")
-    work_time, break_time = opt.optimize(explanatory_variable, objective_variable)
+    work_time, break_time = opt.optimize_round(explanatory_variable, objective_variable)
     # work_time, break_time = optimize_round(explanatory_variable, objective_variable)
     print("提案された作業時間: ", work_time)
     print("提案された休憩時間: ", break_time)
@@ -44,4 +44,40 @@ def round_optimizer(user_id: str, focus_score: float):
     return {
         "work_time": work_time,
         "break_time": break_time
+    }
+
+@app.get("/session_optimizer/{user_id}")
+def session_optimizer(user_id: str, average_focus_score: float):
+    """セッション最適化API
+
+    Args:
+        average_focus_score (float): 集中度スコアの平均
+    Returns:
+        total_work_time (float): 1セッションの作業時間の合計
+        session_break_time (float): 最適なセッション間休憩時間
+        number_of_round (int): 最適なラウンド繰り返し回数
+    """
+    csv_handler = CSVHandler(f"../data/session_csv/{user_id}.csv")
+    # CSVファイル更新
+    new_data = [average_focus_score]
+    columns = ["average_focus_score"]
+    csv_handler.update_data(new_data=new_data, columns=columns)
+    # セッション最適化の説明変数と目的変数を取得
+    explanatory_variable = csv_handler.make_chosen_data_list(columns=["total_work_time", "session_break_time", "number_of_round"])
+    objective_variable = csv_handler.make_chosen_data_list(columns=["average_focus_score"])
+    print("説明変数リスト", explanatory_variable)
+    print("目的変数リスト", objective_variable)
+    # セッション最適化
+    opt = BayesianOptimizer("session")
+    total_work_time, session_break_time, number_of_round = opt.optimize_session(explanatory_variable, objective_variable)
+    print("提案されたセッション間休憩時間: ", session_break_time)
+    print("提案されたラウンド繰り返し回数: ", number_of_round)
+    # CSVファイルの更新
+    new_data = [session_break_time, number_of_round]
+    columns = ["session_break_time", "number_of_round"]
+    csv_handler.update_data(new_data=new_data, columns=columns)
+
+    return {
+        "session_break_time": session_break_time,
+        "number_of_round": int(number_of_round)
     }
--- app/optimize/bayesian_optimizer.py
+++ app/optimize/bayesian_optimizer.py
@@ -1,6 +1,7 @@
 from skopt import Optimizer
 from skopt.learning import GaussianProcessRegressor
 from skopt.learning.gaussian_process.kernels import Matern
+from skopt.space import Integer
 import sys
 sys.path.append("../")
 from csv_handler.csv_handler import CSVHandler
@@ -17,7 +18,7 @@ class BayesianOptimizer:
                 )
             elif target == "session":
                 self.opt = Optimizer(
-                    dimensions=[(60.0, 480.0), (25.0, 60.0), (4, 8)],
+                    dimensions=[(60.0, 480.0), (10.0, 60.0), Integer(1, 8)],
                     base_estimator=GaussianProcessRegressor(kernel=Matern(length_scale=1.0)),
                     n_initial_points=10,
                     acq_func="EI"
@@ -29,7 +30,7 @@ class BayesianOptimizer:
             raise  
     
     
-    def optimize(self, explanatory_variable, objective_variable):
+    def optimize_round(self, explanatory_variable, objective_variable):
         # 目的変数を負の値に変換
         negative_objective = [-v for v in objective_variable]
         # 最適化用データを追加
@@ -39,21 +40,45 @@ class BayesianOptimizer:
 
         return work_time, break_time
     
+    def optimize_session(self, explanatory_variable, objective_variable):
+        # 目的変数を負の値に変換
+        negative_objective = [-v for v in objective_variable]
+        # 最適化用データを追加
+        self.opt.tell(explanatory_variable, negative_objective)
+        # 最適化を実行
+        total_work_time, session_break_time, number_of_round = self.opt.ask()
+
+        return total_work_time, session_break_time, number_of_round
+    
 
 
 if __name__ == "__main__":
     # CSVファイル操作
     csv_handler = CSVHandler("../../data/round_csv/001.csv")
-    # 説明変数と目的変数を取得
+    # ラウンド最適化の説明変数と目的変数を取得
     explanatory_variable = csv_handler.make_chosen_data_list(columns=["work_time", "break_time"])
     objective_variable = csv_handler.make_chosen_data_list(columns=["focus_score"])
     print("説明変数リスト", explanatory_variable)
     print("目的変数リスト", objective_variable)
     # ラウンド最適化
     opt = BayesianOptimizer("round")
-    work_time, break_time = opt.optimize(explanatory_variable, objective_variable)
+    work_time, break_time = opt.optimize_round(explanatory_variable, objective_variable)
     print("提案された作業時間: ", work_time)
     print("提案された休憩時間: ", break_time)
+    
+    
+    # CSVファイル操作
+    csv_handler = CSVHandler("../../data/session_csv/001.csv")
+    # セッション最適化の説明変数と目的変数を取得
+    explanatory_variable = csv_handler.make_chosen_data_list(columns=["total_work_time", "session_break_time, number_of_round"])
+    objective_variable = csv_handler.make_chosen_data_list(columns=["average_focus_score"])
+    print("説明変数リスト", explanatory_variable)
+    print("目的変数リスト", objective_variable)
+    # セッション最適化
+    opt = BayesianOptimizer("session")
+    total_work_time, session_break_time, number_of_round = opt.optimize_session(explanatory_variable, objective_variable)
+    print("提案されたセッション間休憩時間: ", session_break_time)
+    print("提案されたラウンド繰り返し回数: ", number_of_round)
 
 
 
